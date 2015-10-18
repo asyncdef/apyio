795b455198beab7bc606eb53cc51a583bfb54ecf
Kevin Conway <kevinjacobconway@gmail.com>
2015-10-17 20:44:54 -0500

    Add docstrings to tests

--- tests/test_aio.py
+++ tests/test_aio.py
@@ -26,30 +26,35 @@ def async_test(loop=None):
 
 
 def test_string_io_produces_string_io():
+    """Check that the return value is an implementation of the interface."""
     stream = apyio.StringIO()
     assert isinstance(stream, apyio.AsyncStringIOWrapper)
     assert isinstance(stream._stream, io.StringIO)
 
 
 def test_bytes_io_produces_bytes_io():
+    """Check that the return value is an implementation of the interface."""
     stream = apyio.BytesIO()
     assert isinstance(stream, apyio.AsyncBytesIOWrapper)
     assert isinstance(stream._stream, io.BytesIO)
 
 
 def test_async_methods_return_coroutine():
+    """Check that the async read returns a coroutine object."""
     stream = apyio.StringIO()
     coro = stream.read()
     assert hasattr(coro, '__await__')
 
 
 def test_sync_methods_dont_return_coroutine():
+    """Check that the async write does not return a coroutine object."""
     stream = apyio.BytesIO()
     assert not hasattr(stream.write(b''), '__await__')
 
 
 @async_test()
 async def test_async_methods_can_be_awaited():
+    """Check that the async reads can be awaited."""
     stream = apyio.StringIO()
     value = await stream.read()
     assert value == ''
@@ -57,6 +62,7 @@ async def test_async_methods_can_be_awaited():
 
 @async_test()
 async def test_streams_are_async_iterable():
+    """Check that the streams are async iterable."""
     stream = apyio.StringIO()
     stream.writelines(('test\n', 'test2\n'))
     await stream.drain()
@@ -73,6 +79,7 @@ async def test_streams_are_async_iterable():
 
 @async_test()
 async def test_streams_are_async_contexts(tmpdir):
+    """Check that the streams are async context managers."""
     async with apyio.open(str(tmpdir.join('testfile.txt')), 'w') as test_file:
 
         assert test_file
